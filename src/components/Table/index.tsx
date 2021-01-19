9e196c53468bebb12e9033e75bdcc7165e4272e2
Ilya <dnamix1@gmail.com>
2021-01-19 11:16:32 +0300

    cryptocoins table modified

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,31 +1,73 @@
 import * as React from 'react';
 import axios from 'axios';
-import { DataGrid, ColDef, RowModel } from '@material-ui/data-grid';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import {
+  withStyles,
+  Theme,
+  createStyles,
+  makeStyles,
+} from '@material-ui/core/styles';
+
+const StyledTableCell = withStyles((theme: Theme) =>
+  createStyles({
+    head: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    body: {
+      fontSize: 14,
+    },
+  }),
+)(TableCell);
+
+const StyledTableRow = withStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.action.hover,
+      },
+    },
+  }),
+)(TableRow);
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: '100%',
+    minHeight: '100%',
+  },
+  coinImage: {
+    width: 40,
+  },
+  cointPriceChange: {
+    textAlign: 'center',
+    width: '110%',
+    color: 'whitesmoke',
+    fontWeight: 900,
+  },
+});
 
 export default function DataTable() {
+  const classes = useStyles();
   const [coins, setCoins] = React.useState();
-  const [columns, setColumns] = React.useState<ColDef[]>([
-    { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'coinName', headerName: 'Coin name', width: 140 },
-    { field: 'majorMarket', headerName: 'Major Exchange', width: 170 },
-    {
-      field: 'lastPrice',
-      headerName: 'Last Price',
-      type: 'number',
-      width: 130,
-    },
-  ]);
 
-  interface Coins {
-    id: number;
+  console.log(coins);
+
+  interface Coin {
+    image: string;
+    id: string;
     coinName: string;
     majorMarket: string;
     lastPrice: number;
+    dailyChange: number;
   }
-  interface setRowsInterface extends RowModel {
-    coinsRows: Coins[];
-  }
-  const [rows, setRows] = React.useState<setRowsInterface | Coins[]>([]);
+
+  const [rows, setRows] = React.useState<Coin[] | null>([]);
 
   const getCryptoData = async () => {
     const {
@@ -42,18 +84,17 @@ export default function DataTable() {
       setCoins(data);
     });
   }, []);
-  console.log(coins);
 
   React.useEffect(() => {
     // @ts-ignore
-    const coinsRows: Coins[] = coins?.map(coin => {
+    const coinsRows: Coin[] = coins?.map(coin => {
       return {
-        id: +coin.CoinInfo.Id,
-        coinName: coin.CoinInfo.Internal,
+        image: `https://cryptocompare.com${coin.CoinInfo.ImageUrl}`,
+        id: coin.CoinInfo.Internal,
+        coinName: coin.CoinInfo.FullName,
         majorMarket: coin.RAW.USD.LASTMARKET,
-        lastPrice: `${coin.RAW.USD.PRICE} ${
-          coin.RAW.USD.CHANGEHOUR < 0 ? '↘' : '↗'
-        }`,
+        lastPrice: coin.RAW.USD.PRICE,
+        dailyChange: coin.RAW.USD.CHANGE24HOUR,
       };
     });
     console.log(coinsRows);
@@ -64,8 +105,77 @@ export default function DataTable() {
 
   return (
     <div style={{ height: '100%', width: '100%' }}>
-      {/* @ts-ignore */}
-      {rows && <DataGrid rows={rows} columns={columns} autoPageSize />}
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label='customized table'>
+          <TableHead>
+            <TableRow>
+              <StyledTableCell style={{ borderRight: '1px solid grey' }}>
+                Coin Logo
+              </StyledTableCell>
+              <StyledTableCell
+                align='center'
+                style={{ borderRight: '1px solid grey' }}>
+                Ticker
+              </StyledTableCell>
+              <StyledTableCell
+                align='center'
+                style={{ borderRight: '1px solid grey' }}>
+                Coin FullName
+              </StyledTableCell>
+              <StyledTableCell
+                align='center'
+                style={{ borderRight: '1px solid grey' }}>
+                Major Exchange
+              </StyledTableCell>
+              <StyledTableCell
+                align='center'
+                style={{ borderRight: '1px solid grey' }}>
+                Last Price
+              </StyledTableCell>
+              <StyledTableCell align='center'>24 Hour Change</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {/* @ts-ignore */}
+            {rows.map(coin => (
+              <StyledTableRow key={coin.id}>
+                <StyledTableCell scope='row'>
+                  <img src={coin.image} className={classes.coinImage} />
+                </StyledTableCell>
+                <StyledTableCell align='right' style={{ fontWeight: 700 }}>
+                  {coin.id}
+                </StyledTableCell>
+                <StyledTableCell align='right' style={{ fontWeight: 700 }}>
+                  {coin.coinName}
+                </StyledTableCell>
+                <StyledTableCell align='right' style={{ fontWeight: 700 }}>
+                  {coin.majorMarket}
+                </StyledTableCell>
+                <StyledTableCell
+                  align='right'
+                  style={{
+                    width: '110%',
+                    textAlign: 'center',
+                    fontWeight: 900,
+                  }}>
+                  $ {coin.lastPrice}
+                </StyledTableCell>
+                <StyledTableCell
+                  align='right'
+                  className={classes.cointPriceChange}
+                  style={{
+                    background:
+                      coin.dailyChange > 0
+                        ? 'linear-gradient(-45deg, #3FC24A, green)'
+                        : 'linear-gradient(-45deg, #EB4726, red)',
+                  }}>
+                  ${coin.dailyChange.toFixed(2)}
+                </StyledTableCell>
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 }
