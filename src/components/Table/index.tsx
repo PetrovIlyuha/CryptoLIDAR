377b2840ed93fa16d1a18e394f90419ade84d7bb
Ilya <dnamix1@gmail.com>
2021-01-20 14:17:44 +0300

    passed coins props typed into table component

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import axios from 'axios';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -13,6 +12,8 @@ import {
   createStyles,
   makeStyles,
 } from '@material-ui/core/styles';
+import Spinner from '../ui/Spinner';
+import { Coin } from '../../types';
 
 const StyledTableCell = withStyles((theme: Theme) =>
   createStyles({
@@ -40,11 +41,12 @@ const useStyles = makeStyles({
   table: {
     minWidth: '100%',
     minHeight: '100%',
+    cursor: 'pointer',
   },
   coinImage: {
     width: 40,
   },
-  cointPriceChange: {
+  coinPriceChange: {
     textAlign: 'center',
     width: '110%',
     color: 'whitesmoke',
@@ -52,56 +54,12 @@ const useStyles = makeStyles({
   },
 });
 
-export default function DataTable() {
+interface TableProps {
+  coins: Coin[] | null;
+}
+
+export default function CurrenciesList({ coins }: TableProps) {
   const classes = useStyles();
-  const [coins, setCoins] = React.useState();
-
-  console.log(coins);
-
-  interface Coin {
-    image: string;
-    id: string;
-    coinName: string;
-    majorMarket: string;
-    lastPrice: number;
-    dailyChange: number;
-  }
-
-  const [rows, setRows] = React.useState<Coin[] | null>([]);
-
-  const getCryptoData = async () => {
-    const {
-      data: { Data: coinsData },
-    } = await axios.get(
-      `https://min-api.cryptocompare.com/data/top/totalvolfull?limit=10&tsym=USD&api_key=${process.env.REACT_APP_API_KEY}`,
-    );
-    return coinsData;
-  };
-
-  React.useEffect(() => {
-    const marketCaps = getCryptoData();
-    marketCaps.then(data => {
-      setCoins(data);
-    });
-  }, []);
-
-  React.useEffect(() => {
-    // @ts-ignore
-    const coinsRows: Coin[] = coins?.map(coin => {
-      return {
-        image: `https://cryptocompare.com${coin.CoinInfo.ImageUrl}`,
-        id: coin.CoinInfo.Internal,
-        coinName: coin.CoinInfo.FullName,
-        majorMarket: coin.RAW.USD.LASTMARKET,
-        lastPrice: coin.RAW.USD.PRICE,
-        dailyChange: coin.RAW.USD.CHANGE24HOUR,
-      };
-    });
-    console.log(coinsRows);
-    if (coinsRows) {
-      setRows([...coinsRows]);
-    }
-  }, [coins]);
 
   return (
     <div style={{ height: '100%', width: '100%' }}>
@@ -137,42 +95,46 @@ export default function DataTable() {
           </TableHead>
           <TableBody>
             {/* @ts-ignore */}
-            {rows.map(coin => (
-              <StyledTableRow key={coin.id}>
-                <StyledTableCell scope='row'>
-                  <img src={coin.image} className={classes.coinImage} />
-                </StyledTableCell>
-                <StyledTableCell align='right' style={{ fontWeight: 700 }}>
-                  {coin.id}
-                </StyledTableCell>
-                <StyledTableCell align='right' style={{ fontWeight: 700 }}>
-                  {coin.coinName}
-                </StyledTableCell>
-                <StyledTableCell align='right' style={{ fontWeight: 700 }}>
-                  {coin.majorMarket}
-                </StyledTableCell>
-                <StyledTableCell
-                  align='right'
-                  style={{
-                    width: '110%',
-                    textAlign: 'center',
-                    fontWeight: 900,
-                  }}>
-                  $ {coin.lastPrice}
-                </StyledTableCell>
-                <StyledTableCell
-                  align='right'
-                  className={classes.cointPriceChange}
-                  style={{
-                    background:
-                      coin.dailyChange > 0
-                        ? 'linear-gradient(-45deg, #3FC24A, green)'
-                        : 'linear-gradient(-45deg, #EB4726, red)',
-                  }}>
-                  ${coin.dailyChange.toFixed(2)}
-                </StyledTableCell>
-              </StyledTableRow>
-            ))}
+            {!coins ? (
+              <Spinner />
+            ) : (
+              coins.map(coin => (
+                <StyledTableRow key={coin.id}>
+                  <StyledTableCell scope='row'>
+                    <img src={coin.image} className={classes.coinImage} />
+                  </StyledTableCell>
+                  <StyledTableCell align='right' style={{ fontWeight: 700 }}>
+                    {coin.id}
+                  </StyledTableCell>
+                  <StyledTableCell align='right' style={{ fontWeight: 700 }}>
+                    {coin.coinName}
+                  </StyledTableCell>
+                  <StyledTableCell align='right' style={{ fontWeight: 700 }}>
+                    {coin.majorMarket}
+                  </StyledTableCell>
+                  <StyledTableCell
+                    align='right'
+                    style={{
+                      width: '110%',
+                      textAlign: 'center',
+                      fontWeight: 900,
+                    }}>
+                    $ {coin.lastPrice}
+                  </StyledTableCell>
+                  <StyledTableCell
+                    align='right'
+                    className={classes.coinPriceChange}
+                    style={{
+                      background:
+                        coin.dailyChange > 0
+                          ? 'linear-gradient(-45deg, #3FC24A, green)'
+                          : 'linear-gradient(-45deg, #EB4726, red)',
+                    }}>
+                    ${coin.dailyChange.toFixed(2)}
+                  </StyledTableCell>
+                </StyledTableRow>
+              ))
+            )}
           </TableBody>
         </Table>
       </TableContainer>
