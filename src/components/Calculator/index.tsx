303104070017eba5320ba799f465449fddaee32f
Ilya <dnamix1@gmail.com>
2021-01-20 14:15:38 +0300

    add exchange rates calculation logic among two selected cryptocurrencies

--- src/components/Calculator/index.tsx
+++ src/components/Calculator/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
-import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import { Box, Grid, makeStyles, Paper, Typography } from '@material-ui/core';
+import { Currency } from '../../types';
 
 const useStyles = makeStyles(theme => ({
   exchangeArea: {
@@ -21,10 +21,20 @@ const useStyles = makeStyles(theme => ({
     marginBottom: 15,
   },
   currencySelectorField: {
+    color: 'white',
     '& .MuiFormHelperText-root': {
       fontSize: '0.7rem',
       width: '100%',
       marginLeft: 0,
+      fontFamily: 'Righteous',
+    },
+    '& .MuiInputBase-input ': {
+      backgroundColor: '#27346C',
+      color: 'white',
+      borderRadius: 10,
+    },
+    '& .MuiFormLabel-root': {
+      color: 'lightgreen',
       fontFamily: 'Righteous',
     },
   },
@@ -37,6 +47,16 @@ const useStyles = makeStyles(theme => ({
     fontFamily: 'Poppins',
   },
   amountInput: {
+    webkitOuterSpinButton: {
+      webkitAppearance: 'none',
+      margin: 0,
+    },
+    webkitInnerSpinButton: {
+      webkitAppearance: 'none',
+      margin: 0,
+    },
+    mozAppearance: 'textfield',
+
     '& .MuiFormLabel-root': {
       color: 'lightgreen',
       fontFamily: 'Righteous',
@@ -52,40 +72,62 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-const data = [
-  {
-    value: 'USD',
-    label: '$',
-  },
-  {
-    value: 'EUR',
-    label: '€',
-  },
-  {
-    value: 'BTC',
-    label: '฿',
-  },
-  {
-    value: 'JPY',
-    label: '¥',
-  },
-];
+interface CalcProps {
+  currencies: Currency[] | null;
+}
 
-const Calculator = () => {
+const Calculator = ({ currencies }: CalcProps) => {
   const classes = useStyles();
-  const [currencies, setCurrencies] = React.useState({
-    currency1: '',
-    currency2: '',
+  const currencyData = currencies?.map(coin => {
+    return {
+      value: coin.fullname,
+      label: coin.name,
+      price: coin.price,
+    };
   });
 
-  const { currency1, currency2 } = currencies;
+  const defaultCoin1 = currencyData && currencyData[0];
+  const defaultCoin2 = currencyData && currencyData[1];
+  const [selectedCurrency, setSelectedCurrency] = React.useState({
+    currency1: { ...defaultCoin2 },
+    currency2: { ...defaultCoin1 },
+  });
+  const [amount, setAmount] = React.useState({
+    asset1: 0,
+    asset2: 0,
+  });
+
+  const { currency1, currency2 } = selectedCurrency;
+  const { asset1, asset2 } = amount;
 
   const handleChange = (name: string) => (
     event: React.ChangeEvent<HTMLInputElement>,
   ) => {
-    console.log(name);
-    setCurrencies({ ...currencies, [name]: event.target.value });
+    const currentlySetAsset = currencyData?.find(
+      asset => asset.value === event.target.value,
+    );
+    setSelectedCurrency({ ...selectedCurrency, [name]: currentlySetAsset });
   };
+
+  const recalculateRates = (
+    e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>,
+    asset: string,
+  ) => {
+    if (asset === 'asset1') {
+      let asset2Amount = +(
+        (parseInt(e.target.value) * currency1.price) /
+        currency2.price
+      ).toFixed(2);
+      setAmount({ asset1: parseInt(e.target.value), asset2: asset2Amount });
+    } else {
+      let asset1Amount = +(
+        (parseInt(e.target.value) * currency2.price) /
+        currency1.price
+      ).toFixed(2);
+      setAmount({ asset2: parseInt(e.target.value), asset1: asset1Amount });
+    }
+  };
+
   return (
     <Paper elevation={2} className={classes.exchangeArea}>
       <Typography variant='h5' className={classes.calcHeader}>
@@ -100,7 +142,7 @@ const Calculator = () => {
               </Typography>
             )}
             <Typography variant='body1' className={classes.currencyTitle}>
-              {currency1 && `Selected: ${currency1}`}
+              {currency1 && `Selected: ${currency1.value}`}
             </Typography>
           </Box>
         </Grid>
@@ -108,6 +150,9 @@ const Calculator = () => {
           <TextField
             variant='filled'
             label='Amount'
+            type='number'
+            value={asset1}
+            onChange={e => recalculateRates(e, 'asset1')}
             className={classes.amountInput}></TextField>
         </Grid>
         <Grid item md={5}>
@@ -120,9 +165,9 @@ const Calculator = () => {
             helperText='Select currency'
             className={classes.currencySelectorField}
             variant='filled'>
-            {data.map(option => (
+            {currencyData?.map(option => (
               <MenuItem key={option.value} value={option.value}>
-                {option.label}
+                {option.value}
               </MenuItem>
             ))}
           </TextField>
@@ -137,7 +182,7 @@ const Calculator = () => {
               </Typography>
             )}
             <Typography variant='body1' className={classes.currencyTitle}>
-              {currency2 && `Selected: ${currency2}`}
+              {currency2 && `Selected: ${currency2.value}`}
             </Typography>
           </Box>
         </Grid>
@@ -145,6 +190,9 @@ const Calculator = () => {
           <TextField
             variant='filled'
             label='Amount'
+            type='number'
+            onChange={e => recalculateRates(e, 'asset2')}
+            value={asset2}
             className={classes.amountInput}></TextField>
         </Grid>
         <Grid item md={5}>
@@ -157,9 +205,9 @@ const Calculator = () => {
             onChange={handleChange('currency2')}
             helperText='Select currency'
             variant='filled'>
-            {data.map(option => (
+            {currencyData?.map(option => (
               <MenuItem key={option.value} value={option.value}>
-                {option.label}
+                {option.value}
               </MenuItem>
             ))}
           </TextField>
