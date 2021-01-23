d57cbe7a7106947fe48c2de709de4eea3c77f079
Ilya <dnamix1@gmail.com>
2021-01-23 09:52:27 +0300

    add displaying visual indication in colors for price dynamics

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
 import { inject, observer } from 'mobx-react';
 import CurrencyStore from '../../stores/currencyStore';
+import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import Tooltip from '@material-ui/core/Tooltip';
 import {
   withStyles,
   Theme,
@@ -15,6 +16,8 @@ import {
   makeStyles,
 } from '@material-ui/core/styles';
 import Spinner from '../ui/Spinner';
+import { autorun } from 'mobx';
+import { Typography } from '@material-ui/core';
 
 const StyledTableCell = withStyles((theme: Theme) =>
   createStyles({
@@ -48,6 +51,7 @@ const useStyles = makeStyles({
     transition: 'all 0.15s ease-in-out',
     '&:hover': {
       backgroundColor: '#E7DEDD',
+      border: '4px solid darkgrey',
     },
   },
   coinImage: {
@@ -61,6 +65,17 @@ const useStyles = makeStyles({
   },
 });
 
+const HtmlTooltip = withStyles((theme: Theme) => ({
+  tooltip: {
+    backgroundColor: '#333370',
+    color: 'rgba(243, 237, 237, 0.87)',
+    maxWidth: 220,
+    textAlign: 'center',
+    fontSize: theme.typography.pxToRem(12),
+    border: '1px solid #dadde9',
+  },
+}))(Tooltip);
+
 interface TableProps {
   currenciesStore?: CurrencyStore;
 }
@@ -69,13 +84,17 @@ const CurrenciesList = inject('currenciesStore')(
   observer(({ currenciesStore }: TableProps) => {
     const classes = useStyles();
     const coins = currenciesStore!.getCoins;
-    console.log(coins);
-
+    const visualChanges = currenciesStore!.getDiffAssets;
+    console.log(visualChanges);
     React.useEffect(() => {
       if (currenciesStore) {
         currenciesStore.getCryptoData();
+        setInterval(() => {
+          console.log('calling api');
+          currenciesStore.getCryptoData();
+        }, 60 * 1000);
       }
-    }, [currenciesStore]);
+    }, []);
 
     return (
       <div style={{ height: '100%', width: '100%' }}>
@@ -115,42 +134,67 @@ const CurrenciesList = inject('currenciesStore')(
                 <Spinner />
               ) : (
                 coins.map(coin => (
-                  <StyledTableRow
-                    key={coin.id}
-                    className={classes.coinTableRow}>
-                    <StyledTableCell scope='row'>
-                      <img src={coin.image} className={classes.coinImage} />
-                    </StyledTableCell>
-                    <StyledTableCell align='right' style={{ fontWeight: 700 }}>
-                      {coin.id}
-                    </StyledTableCell>
-                    <StyledTableCell align='right' style={{ fontWeight: 700 }}>
-                      {coin.coinName}
-                    </StyledTableCell>
-                    <StyledTableCell align='right' style={{ fontWeight: 700 }}>
-                      {coin.majorMarket}
-                    </StyledTableCell>
-                    <StyledTableCell
-                      align='right'
-                      style={{
-                        width: '110%',
-                        textAlign: 'center',
-                        fontWeight: 900,
-                      }}>
-                      $ {coin.lastPrice}
-                    </StyledTableCell>
-                    <StyledTableCell
-                      align='right'
-                      className={classes.coinPriceChange}
-                      style={{
-                        background:
-                          coin.dailyChange > 0
-                            ? 'linear-gradient(-45deg, #3FC24A, green)'
-                            : 'linear-gradient(-45deg, #EB4726, #972519)',
-                      }}>
-                      ${coin.dailyChange.toFixed(2)}
-                    </StyledTableCell>
-                  </StyledTableRow>
+                  <HtmlTooltip
+                    title={
+                      <React.Fragment>
+                        <Typography color='inherit' variant='caption'>
+                          Click to Select as one of a pair to produce an
+                          exchange rate
+                        </Typography>
+                      </React.Fragment>
+                    }>
+                    <StyledTableRow
+                      key={coin.id}
+                      className={classes.coinTableRow}>
+                      <StyledTableCell scope='row'>
+                        <img src={coin.image} className={classes.coinImage} />
+                      </StyledTableCell>
+                      <StyledTableCell
+                        align='right'
+                        style={{ fontWeight: 700 }}>
+                        {coin.id}
+                      </StyledTableCell>
+                      <StyledTableCell
+                        align='right'
+                        style={{ fontWeight: 700 }}>
+                        {coin.coinName}
+                      </StyledTableCell>
+                      <StyledTableCell
+                        align='right'
+                        style={{ fontWeight: 700 }}>
+                        {coin.majorMarket}
+                      </StyledTableCell>
+                      <StyledTableCell
+                        align='right'
+                        style={{
+                          width: '110%',
+                          textAlign: 'center',
+                          fontWeight: 900,
+                          backgroundColor: `${
+                            visualChanges
+                              ? visualChanges[coin.coinName] === 'rising'
+                                ? '#E56A5A'
+                                : visualChanges[coin.coinName] === 'falling'
+                                ? '#62CD54'
+                                : '#D9E9D7'
+                              : 'lightgrey'
+                          }`,
+                        }}>
+                        $ {coin.lastPrice}
+                      </StyledTableCell>
+                      <StyledTableCell
+                        align='right'
+                        className={classes.coinPriceChange}
+                        style={{
+                          background:
+                            coin.dailyChange > 0
+                              ? 'linear-gradient(-45deg, #3FC24A, green)'
+                              : 'linear-gradient(-45deg, #EB4726, #972519)',
+                        }}>
+                        ${coin.dailyChange.toFixed(2)}
+                      </StyledTableCell>
+                    </StyledTableRow>
+                  </HtmlTooltip>
                 ))
               )}
             </TableBody>
